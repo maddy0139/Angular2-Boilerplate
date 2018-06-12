b2d52e72e428c9400235587116fca599555b5910
Mahendra Gohel <mahendra.gohel@asiapac.local>
2018-06-12 17:39:01 +0530

    Angular2 Boilerplate for beginners

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit  } from '@angular/core';
+import { UserService } from './services/user.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'Angular Rest Example!';
+  users;
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit() {
+    this.userService.getUsers().subscribe(p=>this.users = p);
+ }
+}
